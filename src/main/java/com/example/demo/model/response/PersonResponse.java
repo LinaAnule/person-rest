e3de8f7d995f8a5e273be497b8d7d7b4d0021aa7
elki-hub <82898312+elki-hub@users.noreply.github.com>
2024-03-12 18:39:23 +0200

    Update PersonResponse.java

--- src/main/java/com/example/demo/model/response/PersonResponse.java
+++ src/main/java/com/example/demo/model/response/PersonResponse.java
@@ -1,13 +1,18 @@
 package com.example.demo.model.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class PersonResponse {
+    @Schema(description = "person name", example = "Vardenis")
     private String firstName;
+    @Schema(description = "person last name", example = "Pavardenis")
     private String lastName;
+    @Schema(description = "email", example = "a@mial.com")
     private String email;
+    @Schema(description = "phone", example = "+37061111111")
     private String phone;
 }

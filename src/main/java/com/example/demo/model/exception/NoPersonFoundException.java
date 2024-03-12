30f32076ceb51d7353f9f7b43d044ead5b9e69c7
elki-hub <82898312+elki-hub@users.noreply.github.com>
2024-03-12 18:38:37 +0200

    Update NoPersonFoundException.java

--- src/main/java/com/example/demo/model/exception/NoPersonFoundException.java
+++ src/main/java/com/example/demo/model/exception/NoPersonFoundException.java
@@ -1,4 +1,8 @@
 package com.example.demo.model.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No person was found")
 public class NoPersonFoundException extends RuntimeException{
 }

404001f6a381464a9ead5f31d7aed5910baf1736
elki-hub <82898312+elki-hub@users.noreply.github.com>
2024-03-12 18:39:01 +0200

    Update PersonRequest.java

--- src/main/java/com/example/demo/model/request/PersonRequest.java
+++ src/main/java/com/example/demo/model/request/PersonRequest.java
@@ -1,14 +1,26 @@
 package com.example.demo.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 public class PersonRequest {
+
+    @NonNull
+    @Size(min = 3, max=20)
+    @Schema(description = "Person first name", example = "Vardenis")
     private String firstName;
-    private String middleName;
+
+    @Schema(description = "Person last name", example = "Pavardenis")
     private String lastName;
+
+    @Schema(description = "Person email", example = "email@gmail.com")
     private String email;
+
+    @Schema(description = "Person phone", example = "+37061111111")
     private String phone;
 }
